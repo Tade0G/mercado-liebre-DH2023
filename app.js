687aa5a04dd4d35dc4743bec8af767fc4a21bc1b
Tade0G <tadeogomez1988@gmail.com>
2023-11-04 18:40:23 -0300

    definiendo archivos estaticos en app.js

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const app = express();
 //requerimos path
 const path = require("path");
 
+//definimos lugar de archvos estaticos
+app.use(express.static(path.resolve(__dirname,"./public")));
+
+//configuramos servidor
 app.listen(3000,avisoServidor);
+
+
 
 //funciones 
 
-//avisoServidor
-//funcion: muestra por consola "Servidor levantado"
-//by GomezLucas
+//funcion: avisoServidor
+//def: muestra por consola "Servidor levantado"
+//by Tade0G
 function avisoServidor(){
 	console.log("Servidor levantado");
 }
